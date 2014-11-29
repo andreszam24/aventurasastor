031f3c56a5c1a289a0aa0ca800704684f53d8af5
Andres Zambrano <andreszambrano18@yahoo.es>
2014-11-29 09:04:49 -0500

    splash 100% no title bar

--- Astor/app/src/main/java/com/example/andres/astor/MyActivity.java
+++ Astor/app/src/main/java/com/example/andres/astor/MyActivity.java
@@ -1,12 +1,13 @@
 package com.example.andres.astor;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MyActivity extends ActionBarActivity {
+public class MyActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
